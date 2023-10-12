47f7cc395d09b7dce9327f44d155ef9504445374
Soubhik_Gon <b422056@iiit-bh.ac.in>
2023-10-12 12:44:07 +0000

    appbar and body text

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,24 @@
+import 'package:first_flutter_project/home_view.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+//material.dart is the most important import of the project
+//
+void main() {
+  runApp(const MyApp());
+}
 
+//Flutter Stateless Widget (stl..)
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  //widgets are small puzzles,small pieces which make up the app
+  //once built,you cannot change : stateless
+  //Stateful : consists of data
+  //
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, Soubhik !!',
-        ),
-      ),
+      home: HomeView(),
     );
   }
 }
